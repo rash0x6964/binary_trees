12b83b88cfc8f9e87af7803dfa9429b2e63cdae2
rash0x6964 <rash0x6964@gmail.com>
2024-03-26 18:43:25 +0000

    task 7

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,12 +5,12 @@
  * @tree:  is a pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: Nothing
-*/
+ */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
-		return ;
+		return;
 
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
