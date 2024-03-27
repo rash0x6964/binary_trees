e392c8705f516340f3164f9168f4a9520112aca0
rash0x6964 <rash0x6964@gmail.com>
2024-03-27 15:01:55 +0000

    task 15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,21 +18,23 @@ size_t max(size_t a, size_t b)
  * calc_hight - get the hight of a tree
  * @tree: pointer to the tree
  * Return: Number
-*/
+ */
 
-size_t calc_hight(binary_tree_t *tree) {
+size_t calc_hight(binary_tree_t *tree)
+{
 	int left_height, right_height;
 
-    if (!tree)
-        return 0;
-    left_height = calc_hight(tree->left);
-    right_height = calc_hight(tree->right);
-    return 1 + max(left_height, right_height);
+	if (!tree)
+		return (0);
+	left_height = calc_hight(tree->left);
+	right_height = calc_hight(tree->right);
+	return (1 + max(left_height, right_height));
 }
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: is a pointer to the root node of the tree to measure the balance factor
+ * @tree: is a pointer to the root node of the tree to measure
+ * the balance factor
  * Return: Number
  */
 
